ec8a8d2599594501119b88c583f0034c303eb3b8
finashin82 <finashin82@mail.ru>
2025-09-06 19:28:56 +0900

    Делаю смену оружия и анимации

--- The Zombie Hunter/Assets/Scripts/PlayerMovement.cs
+++ The Zombie Hunter/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private CinemachineCamera freeLookCamera;
+    [SerializeField] private RuntimeAnimatorController[] animatorOverride;
+    [SerializeField] private int numberCurrentAnimator = 0;
 
     private InputData inputData;
     private Animator animator;
@@ -16,19 +18,22 @@ public class PlayerMovement : MonoBehaviour
         inputData = GetComponent<InputData>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        animator.runtimeAnimatorController = animatorOverride[numberCurrentAnimator];
     }
 
     void Update()
     {
         animator.SetFloat("x", inputData.inputVector.x);
         animator.SetFloat("y", inputData.inputVector.y);
+
+        AnimationSprint(inputData.isSprint);
     }
 
     private void FixedUpdate()
     {
         RotationBehindCamera();
 
-        rb.MovePosition(rb.position + moveDirection * speed * Time.deltaTime);
+        PlayerMove();
     }
 
     /// <summary>
@@ -52,5 +57,21 @@ public class PlayerMovement : MonoBehaviour
 
         // Вычисляем направление движения относительно камеры
         moveDirection = (cameraForward * inputData.inputVector.y + cameraRight * inputData.inputVector.x).normalized;
+    }
+
+    /// <summary>
+    /// Движение игрока
+    /// </summary>
+    private void PlayerMove()
+    {
+        rb.MovePosition(rb.position + moveDirection * speed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Анимация ускорения
+    /// </summary>
+    private void AnimationSprint(bool isSprint)
+    {
+        animator.SetBool("isSprint", isSprint);
     }
 }
